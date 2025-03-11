9afa5afb5f99bee656697cdcb39114ede6595e30
Adwita Gadre <adwitagadre@gmail.com>
2025-03-11 13:33:51 -0500

    created connections for Settings UI elements

--- TimeLine/SettingsViewController.swift
+++ TimeLine/SettingsViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBAction func FontSelectionButton(_ sender: Any) {
+    }
+    
+    @IBAction func colorSelectionSegControl(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
