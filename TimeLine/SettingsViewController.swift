fb50ef1c1e4071fb326a2b06668da2c62c92a0a4
Adwita Gadre <adwitagadre@gmail.com>
2025-03-10 23:36:22 -0500

    timeline basic ui

---
+++ TimeLine/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  TimeLine
+//
+//  Created by Adwita Gadre on 3/10/25.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
--- TimeLine/TimelineTableViewCell.swift
+++ TimeLine/TimelineTableViewCell.swift
@@ -23,5 +23,4 @@ class TimelineTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
 }
