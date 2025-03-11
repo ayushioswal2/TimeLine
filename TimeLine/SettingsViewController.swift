eaeda166f836c0047f14b76a9592ad2ae7a26968
Adwita Gadre <adwitagadre@gmail.com>
2025-03-11 13:57:12 -0500

    settings element connections

--- TimeLine/SettingsViewController.swift
+++ TimeLine/SettingsViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBAction func fontSelectionPressed(_ sender: Any) {
+    }
+    @IBOutlet weak var fontSelectionPressed: UIButton!
     @IBAction func FontSelectionButton(_ sender: Any) {
+        
     }
     
     @IBAction func colorSelectionSegControl(_ sender: Any) {
+        
     }
+    
+    @IBOutlet weak var colorSegControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
