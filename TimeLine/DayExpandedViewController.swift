94fd932411499e527edc111b416e5de4d3b811e5
Audrey Chen <audrey.c72135@gmail.com>
2025-03-11 23:02:09 -0500

    day pages base ui done, need to connect segue with main

---
+++ TimeLine/DayExpandedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DayExpandedViewController.swift
+//  TimeLine
+//
+//  Created by Audrey Chen on 3/11/25.
+//
+
+import UIKit
+
+class DayExpandedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.init(red: 255/255, green: 244/255, blue: 225/255, alpha: 1)
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
