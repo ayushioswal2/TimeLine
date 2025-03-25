5849910b0e19963693d8831eb21c76279383dad7
Adwita Gadre <adwitagadre@gmail.com>
2025-03-25 18:25:11 -0500

    settings and timline creation VC font updates

--- TimeLine/TimelineCreationViewController.swift
+++ TimeLine/TimelineCreationViewController.swift
@@ -9,12 +9,36 @@ import UIKit
 
 class TimelineCreationViewController: UIViewController {
 
+    @IBOutlet weak var inviteCreatorsLabel: UILabel!
+    @IBOutlet weak var timelineNameLabel: UILabel!
     @IBOutlet weak var timelineCreationTitleLabel: UILabel!
+    
+    @IBOutlet weak var createTimelineButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateFont), name: NSNotification.Name("FontChanged"), object: nil)
+        
         // Initial Set Up
         view.backgroundColor = UIColor.init(red: 255/255, green: 244/255, blue: 225/255, alpha: 1)
-        timelineCreationTitleLabel.font = UIFont(name: "Refani", size: CGFloat(30))
+        timelineCreationTitleLabel.font = UIFont.appFont(forTextStyle: .title1, weight: .bold)
+        timelineNameLabel.font = UIFont.appFont(forTextStyle: .title3, weight: .medium)
+        inviteCreatorsLabel.font = UIFont.appFont(forTextStyle: .title3, weight: .medium)
+        
+        createTimelineButton.titleLabel?.font = UIFont.appFont(forTextStyle: .headline, weight: .medium)
+        cancelButton.titleLabel?.font = UIFont.appFont(forTextStyle: .body, weight: .medium)
+        sendButton.titleLabel?.font = UIFont.appFont(forTextStyle: .headline, weight: .medium)
+    }
+    
+    @objc func updateFont() {
+        timelineCreationTitleLabel.font = UIFont.appFont(forTextStyle: .title1, weight: .bold)
+        timelineNameLabel.font = UIFont.appFont(forTextStyle: .title3, weight: .medium)
+        inviteCreatorsLabel.font = UIFont.appFont(forTextStyle: .title3, weight: .medium)
+        createTimelineButton.titleLabel?.font = UIFont.appFont(forTextStyle: .headline, weight: .medium)
+        cancelButton.titleLabel?.font = UIFont.appFont(forTextStyle: .body, weight: .medium)
+        sendButton.titleLabel?.font = UIFont.appFont(forTextStyle: .headline, weight: .medium)
     }
 }
