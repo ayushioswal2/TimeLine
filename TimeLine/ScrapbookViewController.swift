e2215afebd18fcd94e668647aa39b20bda358407
Adwita Gadre <adwitagadre@gmail.com>
2025-03-25 19:04:00 -0500

    day page font toggle changes

--- TimeLine/ScrapbookViewController.swift
+++ TimeLine/ScrapbookViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ScrapbookViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateFont), name: NSNotification.Name("FontChanged"), object: nil)
 
         view.backgroundColor = UIColor.init(red: 255/255, green: 244/255, blue: 225/255, alpha: 1)
 
+        dateLabel.font = UIFont.appFont(forTextStyle: .title1, weight: .bold)
+        saveButton.titleLabel?.font = UIFont.appFont(forTextStyle: .body, weight: .regular)
+    }
+    
+    @objc func updateFont() {
+        dateLabel.font = UIFont.appFont(forTextStyle: .title1, weight: .bold)
+        saveButton.titleLabel?.font = UIFont.appFont(forTextStyle: .body, weight: .regular)
     }
 }
