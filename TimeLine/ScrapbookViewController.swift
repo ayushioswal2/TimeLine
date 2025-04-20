368240067c536127d71ef5dc6c11e6d3c7e30f15
ayushioswal2 <ayushioswal-admin>
2025-04-20 12:43:48 -0500

    working on adding drawingi functionality

--- TimeLine/ScrapbookViewController.swift
+++ TimeLine/ScrapbookViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import PencilKit
 
 class ScrapbookViewController: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var canvasUIView: UIView!
+    var drawingCanvasView: PKCanvasView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +29,26 @@ class ScrapbookViewController: UIViewController {
 
         saveButton.titleLabel?.font = UIFont.appFont(forTextStyle: .body, weight: .regular)
         saveButton.backgroundColor = UIColor.appColorScheme(type: "secondary")
+        
+        // make canvas page noticable to the user
+        canvasUIView.backgroundColor = .white
+        canvasUIView.layer.shadowColor = UIColor.black.cgColor
+        canvasUIView.layer.shadowOpacity = 0.2
+        canvasUIView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        canvasUIView.layer.shadowRadius = 8
+        canvasUIView.layer.cornerRadius = 12
+        
+        drawingCanvasView = PKCanvasView(frame: canvasUIView.bounds)
+        drawingCanvasView.backgroundColor = .clear
+        drawingCanvasView.drawingPolicy = .anyInput
+        canvasUIView.addSubview(drawingCanvasView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        // Ensure canvas matches container size
+        drawingCanvasView.frame = canvasUIView.bounds
     }
     
     @objc func updateFont() {
@@ -36,5 +60,23 @@ class ScrapbookViewController: UIViewController {
         dateLabel.textColor = UIColor.appColorScheme(type: "primary")
         saveButton.backgroundColor = UIColor.appColorScheme(type: "secondary")
 
+    }
+    
+    @IBAction func penButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shapeButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func eraseButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func colorButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func addImageButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func addTextButtonPressed(_ sender: Any) {
     }
 }
